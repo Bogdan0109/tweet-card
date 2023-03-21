20a3da77ea8e34944cf0d3e8b711449efeb34184
Bogdan0109 <102528317+Bogdan0109@users.noreply.github.com>
2023-03-21 21:18:12 +0200

    following-and-unfollowing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,28 @@
 import './App.css';
 import imageBoy from '../images/boy.png';
 import svgLogo from '../images/logo.svg';
+import { useState } from 'react';
 
 export const App = () => {
+  const [followers, setFollowers] = useState(100500);
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const onHandlerClickFollow = () => {
+    switch (isFollowing) {
+      case false:
+        setFollowers(followers + 1);
+        setIsFollowing(true);
+        break;
+      case true:
+        setFollowers(followers - 1);
+        setIsFollowing(false);
+        break;
+
+      default:
+        console.log('Invalid subscription type');
+    }
+  };
+
   return (
     <section className="hero">
       <div className="hero__card">
@@ -22,11 +42,20 @@ export const App = () => {
           height="90"
         />
         <div className="hero__svg"></div>
-        <p>777 tweets</p>
-        <p>100,500 Followers</p>
-        <button className="hero__btn" type="button">
-          Follow
-        </button>
+        <div className="hero__info">
+          <p className="hero__tweets">777 tweets</p>
+          <p className="hero__followers">{followers} Followers</p>
+          <button
+            className="hero__btn"
+            style={{
+              backgroundColor: !isFollowing ? '#EBD8FF' : '#5CD3A8',
+            }}
+            type="button"
+            onClick={onHandlerClickFollow}
+          >
+            {!isFollowing ? 'Follow' : 'Following'}
+          </button>
+        </div>
       </div>
     </section>
   );
